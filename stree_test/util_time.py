f352362a37b691e9aad55b6acfccb1aea2e4ab24
Austin Cheng <acheng@netskope.com>
2025-11-12 16:21:32 +0800

    keyboard interrupt

--- stree_test/util_time.py
+++ stree_test/util_time.py
@@ -5,17 +5,20 @@ def sleep_ex(duration_seconds: float):
         time.sleep(duration_seconds)
         return
 
-    end_time = time.time() + duration_seconds
+    try:
+        end_time = time.time() + duration_seconds
 
-    while time.time() < end_time:
-        remaining = end_time - time.time()
-        sleep_interval = 0.0
+        while time.time() < end_time:
+            remaining = end_time - time.time()
+            sleep_interval = 0.0
         
-        if remaining >= 1.0:
-            sleep_interval = 1.0
-        elif remaining > 0:
-            sleep_interval = remaining
-        else:
-            break
+            if remaining >= 1.0:
+                sleep_interval = 1.0
+            elif remaining > 0:
+                sleep_interval = remaining
+            else:
+                break
 
-        time.sleep(sleep_interval)
+            time.sleep(sleep_interval)
+    except KeyboardInterrupt:
+        return
